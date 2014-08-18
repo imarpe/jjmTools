6a8f322b5ac283d318d49061e1d89ce7bfc44b64
ErickChacon <eral.th07@gmail.com>
2014-08-17 19:33:19 -0500

    output-class

--- R/jjm.output-class.R
+++ R/jjm.output-class.R
@@ -1,20 +1,25 @@
 
-.readJjmOutput = function(...) {
+.readJjmOutput = function(outputPath, modelName, ...) {
+  data = readList(file.path(outputPath, paste(modelName,"_R.rep",sep="")))
+  info = list(model = modelName)
+  output = list(data=data, info=info)
   class(output) = c("jjm.output", class(output))
   return(output)
 }
 
 
 print.jjm.output = function(x, ...) {
-  
+  cat("Output data from ", sQuote(x$info$model), "\n", sep="")
 
   return(invisible())
-  
 }
 
 summary.jjm.output = function(object,...) {
   
-
+  output = list()
+  
+  output$info = object$info
+  
   class(output) = "summary.jjm.output"
   return(output)
   
@@ -22,14 +27,15 @@ summary.jjm.output = function(object,...) {
 
 print.summary.jjm.output = function(x, ...) {
   
-
+  x2=x; class(x2)='jjm.output'
+  print(x2,...)
   
   return(invisible(x))
 }
 
-plot.jjm.output = function(x, ,...) {
+plot.jjm.output = function(x, jjm.data, what = c("fit"),...) {
   
+  diagnostics(jjm.out = x$data, jjm.in = jjm.data, jjm.ypr = jjm.ypr, what = what)
 
-  
   return(invisible())
 }
