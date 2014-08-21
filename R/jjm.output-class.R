3914c32a456341aade59d1a431e7bb1507101ad0
roliveros-ramos <ricardo.oliveros@gmail.com>
2014-08-21 18:07:41 -0500

    Update "output" class
    Signed-off-by: roliveros-ramos <ricardo.oliveros@gmail.com>

--- R/jjm.output-class.R
+++ R/jjm.output-class.R
@@ -1,35 +1,58 @@
 
 .getJjmOutput <- function(path = path, model = model, ...) {
+  
   # Define path of input
   inputPath   <- file.path(path, "Code/admb/")
   
   # Read files .rep and .yld
-  output1 <- readList(file.path(inputPath, paste("arc/", model, "_r.rep", sep = "")))
-  output2 <- .readYPR(file.path(inputPath, paste( "arc/", modelName,".yld", sep = "")))
-  info <- list(model = model)
+  output      <- readList(file.path(inputPath, paste("arc/", model, "_R.rep", sep = "")))
+  ypr         <- .readYPR(file.path(inputPath, paste( "arc/", modelName,".yld", sep = "")))
+  
+  # Extract asociated .dat file
+  dataName    <- scan(file = file.path(inputPath, paste0(model, ".ctl")), what = character(), sep = "\n", quiet = TRUE)
+  dataName    <- gsub(x = dataName[1], pattern = " ", replacement = "")
+  
+  # Read .dat file
+  data        <- .read.dat(iPath = inputPath, iFilename = dataName)
+  
+  # Generate extra info
+  iFilename   <- file.path(inputPath, dataName)
+  info.data   <- list(file = iFilename, variables = length(names(output)), year=c(output$years[1], output$years[2]),
+                      age = c(output$ages[1], output$ages[2]), length = c(output$lengths[1], output$lengths[2]))
+  info.output <- list(model = model, fisheryNames = output$Fshry_names, modelYears = output$Yr,
+                      indexModel = output$Index_names)
   
   # Group in a list
-  output <- list(info = info, out = output1, YPR = output2)
+  output <- list(output = list(info = info.output, output = output, YPR = ypr),
+                 data = list(info = info.data, data = data))
   
   # Define jjm.output class
-  class(output) = c("jjm.output", class(output))
+  class(output) <- c("jjm.output", class(output))
   
   return(output)
 }
 
 
 print.jjm.output <- function(x, ...) {
-  # Get name of model
-  cat("Output data from ", sQuote(x$info$model), "\n", sep = "")
-
+  
+  cat("jjm.data from ", sQuote(x$data$info$file), "\n", sep = "")
+  cat("Number of variables: ", x$data$info$variables, "\n", sep = "")
+  cat("Years from: ", x$data$info$year[1] ,"to", x$data$info$year[2], "\n", sep = " ")
+  cat("Ages from: ", x$data$info$age[1] ,"to", x$data$info$age[2], "\n", sep = " ")
+  cat("Lengths from: ", x$data$info$length[1] ,"to", x$data$info$length[2], "\n", sep = " ")
+  
+  cat("Model name: ", x$output$info$model, "\n")
+  cat("Fisheries names: ", paste(x$output$info$fisheryNames, collapse = ", "), "\n")
+  cat("Asociated indices: ", paste(x$output$info$indexModel, collapse = ", "), "\n")
+  
   return(invisible())
 }
 
-summary.jjm.output = function(object,...) {
+summary.jjm.output = function(object, ...) {
   
   output = list()
   
-  output$info <- object$info
+  output$info <- object$output$info
   output$like <- .LikeTable(object)
   output$fut  <- .Fut_SSB_SD(object)
   output$SSB  <- .SSB_SD(object)
@@ -41,10 +64,21 @@ summary.jjm.output = function(object,...) {
 
 print.summary.jjm.output <- function(x, ...) {
   
-  x2        <- x
-  class(x2) <- 'jjm.output'
+  #  x2        <- x
+  #  class(x2) <- 'jjm.output'
   
-  print(x2, ...)
+  #  print(x2, ...)
+  cat("\nModel:\n\n")
+  print(x$info$model, ...)
+  
+  cat("\nFishyNames:\n\n")
+  print(x$info$fishyNames, ...)
+  
+  cat("\nYears:\n\n")
+  print(x$info$modelYears, ...)
+  
+  cat("\nIndex Model:\n\n")
+  print(x$info$indexModel, ...)
   
   cat("\nLikelihood Table:\n\n")
   print(x$like, ...)
