0bb6ae6374f85c5ca04b99b327eb53d15519f0f2
LuisLauM <luis.laum@gmail.com>
2014-08-22 10:11:27 -0500

    Changes in jjmTools-main.R
    Signed-off-by: LuisLauM <luis.laum@gmail.com>

--- R/jjmTools-main.R
+++ R/jjmTools-main.R
@@ -1,12 +1,11 @@
 # To define generic classes and methods for the package
 #.readJjmOutput = function(outputPath, modelName, ...)
-readJJM <- function(path = NULL, modelName, ...) {
-
-  if(is.null(path))
-    path <- paste0(getwd(), "/../..")
+readJJM <- function(path, modelName, ...) {
     
+  # Set lower case for model name
   modelName <- tolower(modelName)
   
+  # Acording to the length of modelname (number of models), decide the way to process
   if(length(modelName) > 1)
     output <- .getJjmOutputS(path = path, listName = modelName) else
       output <- .getJjmOutput(path = path, model = modelName, ...)
@@ -17,9 +16,11 @@ readJJM <- function(path = NULL, modelName, ...) {
 
 diagnostics <- function(outputObject, ...) {
   
+  # Take an output object and get diagnostic plots extracting outputs, data and YPR
   output <- .diagnostics(jjm.out = outputObject$output, jjm.in = outputObject$data$data,
                          jjm.ypr = outputObject$output$YPR)
   
+  # Return a jjm.diag object
   class(output) = c("jjm.diag", class(output))
   return(output)
 }
