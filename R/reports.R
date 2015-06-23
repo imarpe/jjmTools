c325b5232f4790d12e65871da1ad46bfe9728bf6
roliveros-ramos <ricardo.oliveros@gmail.com>
2015-06-23 11:54:21 -0500

    functions for reports + Rmd templates

---
+++ R/reports.R
@@ -0,0 +1,70 @@
+
+report = function(...) {
+  UseMethod("report")
+}
+
+report.jjm.output = function(object, format="latex", Fmult = NULL,
+                             BiomProj = NULL, CapProj = NULL, verbose=TRUE,
+                             MRS = NULL, tangle=FALSE, tidy=TRUE, ...) {
+
+  modelName = deparse(substitute(object))
+  
+  f = system.file("reports", "report-jjm.output.Rmd", package = "jjmTools")
+  knit(f)
+  if(isTRUE(tangle)) {
+    knit(f, tangle=TRUE)
+    f1 = gsub(pattern = ".Rmd", replacement = "\\.R", f)
+    file.rename(from=basename(f1), to=paste0(modelName, ".R"))
+  }
+  f2 = gsub(pattern = ".Rmd", replacement = "\\.md", f)
+  f3 = gsub(pattern = ".Rmd", replacement = "\\.pdf", f)
+  pandoc(basename(f2), format=format)
+  outputFile = paste0(modelName, "_output.pdf")
+  file.rename(from=basename(f3), to=outputFile)
+  
+  if(isTRUE(tidy)) file.remove(basename(f2))
+
+  if(isTRUE(verbose)) shell.exec(outputFile)
+  
+  return(invisible(paste0(modelName, ".pdf")))
+}
+
+report.jjm.diag = function(object, format="latex", tangle=FALSE, tidy=TRUE, verbose=TRUE, ...) {
+  
+  modelName = deparse(substitute(object))
+  
+  f = system.file("reports", "report-jjm.diag.Rmd", package = "jjmTools")
+  knit(f)
+  if(isTRUE(tangle)) {
+    knit(f, tangle=TRUE)
+    f1 = gsub(pattern = ".Rmd", replacement = "\\.R", f)
+    file.rename(from=basename(f1), to=paste0(modelName, ".R"))
+  }
+  f2 = gsub(pattern = ".Rmd", replacement = "\\.md", f)
+  f3 = gsub(pattern = ".Rmd", replacement = "\\.pdf", f)
+  pandoc(basename(f2), format=format)
+  outputFile = paste0(modelName, "_diag.pdf")
+  file.rename(from=basename(f3), to=outputFile)
+  
+  if(isTRUE(tidy)) file.remove(basename(f2))
+  
+  if(isTRUE(verbose)) shell.exec(outputFile)
+  
+  return(invisible(paste0(modelName, ".pdf")))
+  
+  return(invisible())
+}
+
+# report.jjm.diag = function(object, format="latex") {
+#   
+#   pichon = object
+#   f = system.file("reports", "jjmDiag.Rmd", package = "jjmTools")
+#   f = "test.Rmd"
+#   knit(f)
+#   knit(f, tangle=TRUE)
+#   f2 = gsub(pattern = ".Rmd", replacement = "\\.md", f)
+#   pandoc(f2, format=format)
+#   
+# }
+# report(mod1)
+# report(mod2)
