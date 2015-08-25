0fe3d42ab1bcb8bd1a716ff37a89116b09baac33
Enrique Ramos <enrique756@gmail.com>
2015-08-25 14:54:52 -0500

    print.summary.jjm.output (grid)

--- R/jjm.output-class.R
+++ R/jjm.output-class.R
@@ -96,7 +96,8 @@ print.summary.jjm.output = function(x, ...) {
   for(i in seq_along(x$plots)){
     
     cat(names(x$plots)[i], "\n")
-    plot(x$plots[[i]])
+    grid.newpage()
+    grid.draw(x$plots[[i]])
   
   }
   
